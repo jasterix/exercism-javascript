28173e4caf5f2af6144f077e30d5e17f48c89f14
Jasterix <ljcalixte@gmail.com>
2019-09-30 14:25:50 -0400

    need to figure out algortithm for binary conversion

--- all-your-base/all-your-base.js
+++ all-your-base/all-your-base.js
@@ -1,31 +1,47 @@
-
 export const convert = (number, inbase, outbase) => {
-// convert binary to base 10
-let numLength = number.length-1
-let arr = []
-let inbases = [2, 3, 7, 10, 16, 73, 97]
-let outbases = [2, 3, 7, 10, 16, 73, 97]
+  // convert binary to base 10
+  let numLength = number.length - 1
+  let arr = []
+  let inbases = [2, 3, 7, 10, 16, 73, 97]
+  let outbases = [2, 3, 7, 10, 16, 73, 97]
 
-if (!inbases.includes(inbase)) {
-  throw new Error('Wrong input base')
-}
-
-if (!outbases.includes(outbase)) {
-  throw new Error('Wrong output base')
-}
-
-if (number[0]===0 || number.length === 0 || (inbase===2 && number.some(x => 1 < x || x < 0))) {
-  console.log(inbase===2 && number.some(x => 1 < x || x < 0), number)
-  throw new Error("Input has wrong format")
-}
-
-
-  for (let d = 0; d <= numLength; d++){
-    for (let e = numLength; e >= numLength; e--){
-      arr.push(number[d]*(2**(e-d)))
-    }
+  if (!inbases.includes(inbase)) {
+    throw new Error('Wrong input base')
   }
-  let answer = arr.reduce((total, x)=> total+x).toString().split("")
-  return answer.map(x=> +x)
 
+  if (!outbases.includes(outbase)) {
+    throw new Error('Wrong output base')
+  }
+
+  if ((number[0] === 0 && number.length > 1) || number.length === 0 || (inbase === 2 && number.some(x => 1 < x || x < 0))) {
+    throw new Error("Input has wrong format")
+  }
+
+  switch (outbase) {
+    case 2:
+      let num = number.join("")
+      binaryConvert(num)
+      break;
+    default:
+      for (let d = 0; d <= numLength; d++) {
+        for (let e = numLength; e >= numLength; e--) {
+          arr.push(number[d] * (2 ** (e - d)))
+        }
+      }
+      let answer = arr.reduce((total, x) => total + x).toString().split("")
+      return answer.map(x => +x)
+  }
+
+}
+
+const binaryConvert = (num, binary = num, result = "") => {
+  if (binary % 2 < 1) {
+    result.concat("0")
+    console.log(result)
+    return result
+  }
+    console.log(result)
+    result.concat("1")
+    binaryConvert(binary / 2)
+    // return binaryConvert(binary / 2).toString + (binary%2).toString
 }
--- all-your-base/all-your-base.spec.js
+++ all-your-base/all-your-base.spec.js
@@ -9,7 +9,7 @@ describe('Converter', () => {
     expect(convert([1, 0, 1], 2, 10)).toEqual([5]);
   });
 
-  xtest('single decimal to binary', () => {
+  test('single decimal to binary', () => {
     expect(convert([5], 10, 2)).toEqual([1, 0, 1]);
   });
 
@@ -17,7 +17,7 @@ describe('Converter', () => {
     expect(convert([1, 0, 1, 0, 1, 0], 2, 10)).toEqual([4, 2]);
   });
 
-  xtest('decimal to binary', () => {
+  test('decimal to binary', () => {
     expect(convert([4, 2], 10, 2)).toEqual([1, 0, 1, 0, 1, 0]);
   });
 
