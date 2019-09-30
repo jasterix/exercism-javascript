f9421fd5398ab2c19c8d87b11957e52d88cc09c2
Jasterix <ljcalixte@gmail.com>
2019-09-30 17:28:16 -0400

    resistor color refactor based on mentor feedback

--- resistor-color/resistor-color.js
+++ resistor-color/resistor-color.js
@@ -1,5 +1,3 @@
 export const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"]
 
-export const colorCode = (string) => {
-  return COLORS.indexOf(string)
-};
+export const colorCode = (string) => COLORS.indexOf(string)
