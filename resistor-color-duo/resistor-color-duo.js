163b9f602b75e88df7f0beb8253c601f2ee71d20
Jasterix <ljcalixte@gmail.com>
2019-10-01 12:42:55 -0400

    first submission for resistor-color-duo challenge

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -1,3 +1,3 @@
-export const value = () => {
+let colors = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"]
 
-};
+export const value = (arr) => parseInt(arr.map(color => colors.indexOf(color)).slice(0,2).join(""))
