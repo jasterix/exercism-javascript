887b4eb3cff8314acae25880c3c1c5793337b788
Jasterix <ljcalixte@gmail.com>
2019-10-15 14:13:29 -0400

    new solution using prior exercise modules and string literals

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -1,14 +1,7 @@
 import { colorCode, COLORS } from "./../resistor-color/resistor-color.js";
 
 export const value = arr => {
-  let number = arr.map(color => {
-    return colorCode(color);
-  });
-  console.log(number);
-  //   parseInt(
-  //     arr
-  //       .map(color => COLORS.indexOf(color))
-  //       .slice(0, 2)
-  //       .join("")
-  //   );
+  let ans = arr.map(color => colorCode(color));
+
+  return parseInt(`${ans[0]}${ans[1]}`);
 };
