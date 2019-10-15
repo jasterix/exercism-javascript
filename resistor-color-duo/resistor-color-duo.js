af948b759a428756c8b1850fd42dc2243ea661cb
Jasterix <ljcalixte@gmail.com>
2019-10-15 13:55:33 -0400

    attempting to use the colorCode module from prior exercise

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -1,9 +1,12 @@
 import { colorCode, COLORS } from "./../resistor-color/resistor-color.js";
 
-export const value = arr =>
-  parseInt(
-    arr
-      .map(color => COLORS.indexOf(color))
-      .slice(0, 2)
-      .join("")
-  );
+export const value = arr => {
+  console.log((colorCode(arr))
+
+//   parseInt(
+//     arr
+//       .map(color => COLORS.indexOf(color))
+//       .slice(0, 2)
+//       .join("")
+//   );
+}
