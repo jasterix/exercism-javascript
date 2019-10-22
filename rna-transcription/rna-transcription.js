94d21831e14727326699ac8165a87e1866ea7e6c
Jasterix <ljcalixte@gmail.com>
2019-10-22 14:38:02 -0400

    cleaned up solution

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -1,4 +1,3 @@
-// Moreover here, you can with only 1 map to get the solution (no need of the for loop and the Object.entries). Just note, that to cast a string to an array, you can use the spread operator : [...string] and to cast from an array to a string, you can use .join('').
 const transcription = {
   G: "C",
   C: "G",
@@ -13,14 +12,3 @@ export const toRna = string => {
   });
   return result;
 };
-
-// export const toRna = string => {
-//   for (let char of string) {
-//     Object.entries(transcription).map(([key, value]) => {
-//       if (key === char) {
-//         res += value;
-//       }
-//     });
-//   }
-//   return res;
-// };
