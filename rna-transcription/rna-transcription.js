e9f15e16af6c9eae1b7a24ff6f356849ae9c9dc0
Jasterix <ljcalixte@gmail.com>
2019-10-22 13:37:12 -0400

    removed res default arg

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -1,19 +1,30 @@
-// Since res is not really an input, you can declare it inside the function body rather than in its signature!
-
 // Moreover here, you can with only 1 map to get the solution (no need of the for loop and the Object.entries). Just note, that to cast a string to an array, you can use the spread operator : [...string] and to cast from an array to a string, you can use .join('').
 const transcription = {
   G: "C",
   C: "G",
   T: "A",
   A: "U"
-};
-export const toRna = (string, res = "") => {
+}
+let res = ""
+
+export const toRna = string => {
   for (let char of string) {
     Object.entries(transcription).map(([key, value]) => {
       if (key === char) {
-        res += value;
+        res += value
       }
-    });
+    })
   }
-  return res;
-};
+  return res
+}
+
+// export const toRna = string => {
+//   for (let char of string) {
+//     Object.entries(transcription).map(([key, value]) => {
+//       if (key === char) {
+//         res += value;
+//       }
+//     });
+//   }
+//   return res;
+// };
