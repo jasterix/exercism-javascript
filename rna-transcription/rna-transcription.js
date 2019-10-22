3a98f326c00326819abdd7095816ef9716edfc00
Jasterix <ljcalixte@gmail.com>
2019-10-22 14:53:20 -0400

    3rd time refactoring

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -1,3 +1,7 @@
+// Humm... Well, that's not totally it : in fact, try to avoid mutating external data inside the map body this is not a good practice!
+
+// Here you have the right beginning, but rather than using a temporary variable (result), just get the result from the .map operation and as I have indicated, it is an array, so you have to join it to return it!
+
 const transcription = {
   G: "C",
   C: "G",
