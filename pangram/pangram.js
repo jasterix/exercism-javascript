a22e62960c7b0ce4b76c5d9b2e66e286e0ca6934
Jasterix <ljcalixte@gmail.com>
2019-11-07 15:48:04 -0500

    pass all tests

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -1,8 +1,8 @@
 export const isPangram = (sentence) => {
   let lower = sentence.toLowerCase()
-  const alph = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-  if (lower){
-  return [...alph].every(char => lower.includes(char))
-}
-return false
+  const alph = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
+  if (lower) {
+    return [...alph].every(char => lower.includes(char));
+  }
+  return false;
 };
