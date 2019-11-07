1a196138a51a04a7f0baf54cd84b8dc36f08510a
Jasterix <ljcalixte@gmail.com>
2019-11-07 15:11:16 -0500

    start solving pangram

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -1,8 +1,3 @@
-//
-// This is only a SKELETON file for the 'Pangram' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const isPangram = () => {
-  throw new Error("Remove this statement and implement this function");
+
 };
