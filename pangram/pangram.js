2a646dc33e10fcdb7440565b712b8491e2eacf29
Jasterix <ljcalixte@gmail.com>
2019-11-07 16:07:31 -0500

    remove unnecessary spread operator

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -2,7 +2,7 @@ export const isPangram = (sentence) => {
   let lower = sentence.toLowerCase()
   const alph = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
   if (lower) {
-    return [...alph].every(char => lower.includes(char));
+    return alph.every(char => lower.includes(char));
   }
   return false;
 };
