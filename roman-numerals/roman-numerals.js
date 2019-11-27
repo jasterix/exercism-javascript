ada845d36da2a35ced1fe5d60ad5709841734f8d
Jasterix <ljcalixte@gmail.com>
2019-11-26 19:12:39 -0500

    create conversion object

--- roman-numerals/roman-numerals.js
+++ roman-numerals/roman-numerals.js
@@ -1,5 +1,6 @@
 export const toRoman = (number) => {
-  let romans = ["I", "V", "X", "L", "C", "D", "M"]
+  // let romans = ["I", "V", "X", "L", "C", "D", "M"]
+  const conversion = {M:1000,CM:900, D:500,CD:400, C:100, XC:90,L:50, XV: 40, X:10, IX:9, V:5, IV:4, I:1}
   // convert number to array
   //  count the number of digits
   //  convert each number to roman right to left
