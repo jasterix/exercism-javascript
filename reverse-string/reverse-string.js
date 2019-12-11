d782c6ba222aba1aabda43a833bd4dfba24fd294
Jasterix <ljcalixte@gmail.com>
2019-12-10 21:06:40 -0500

    starting solution 3

--- reverse-string/reverse-string.js
+++ reverse-string/reverse-string.js
@@ -1,15 +1,17 @@
-// Initial solution
-// export const reverseString = (string, answer = "") => {
-//   for (let i = string.length; i >= 0; i--){
-//     answer+=string.charAt(i)
+// // Initial solution
+// // export const reverseString = (string, answer = "") => {
+// //   for (let i = string.length; i >= 0; i--){
+// //     answer+=string.charAt(i)
+// //   }
+// //   return answer
+// // };
+//
+// export const reverseString = (string)=> {
+//   let result = ""
+//   for (let i= sring.length; i>= 0; i--){
+//     result += string.charAt(i)
 //   }
-//   return answer
-// };
+//   return result
+  // }
 
-export const reverseString = (string)=> {
-  let result = ""
-  for (let i= sring.length; i>= 0; i--){
-    result += string.charAt(i)
-  }
-  return result
-}
+  
