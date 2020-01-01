6632b3acd318bb987d9fa2ad42294b6ba09f142e
Jasterix <ljcalixte@gmail.com>
2020-01-01 15:04:57 -0500

    remove dead code

--- high-scores/high-scores.js
+++ high-scores/high-scores.js
@@ -1,8 +1,3 @@
-//
-// This is only a SKELETON file for the 'High-Scores' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class HighScores {
   constructor(input) {
     this.input = input
