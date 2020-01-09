33db61f2e8e8963bb173ad4510da77eb7501825d
Jasterix <ljcalixte@gmail.com>
2020-01-08 21:04:35 -0500

    change let to const

--- high-scores/high-scores.js
+++ high-scores/high-scores.js
@@ -8,18 +8,18 @@ export class HighScores {
   }
 
   get latest() {
-    let scores = this.input;
+    const scores = this.input;
     return scores[scores.length - 1];
   }
 
   get personalBest() {
-    let scores = this.input;
+    const scores = this.input;
     return Math.max(...scores);
   }
 
   get personalTopThree() {
-    let scores = this.input;
-    let reverse = [...scores].sort((a, b) => b - a);
+    const scores = this.input;
+    const reverse = [...scores].sort((a, b) => b - a);
     return reverse.splice(0, 3);
   }
 }
